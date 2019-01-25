392235ceabe6cbdc69e08782dc82295a02c1231b
Kewin Dousse <kewin.d@websud.ch>
2019-01-25 22:07:03 +0100

    Base structure

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hi");
+#!/usr/bin/env node
+
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-s, --spotify', 'Use the Spotify Web API')
+  .option('-b, --beets', 'Use Beets')
+  .parse(process.argv);
+
+if (program.spotify) {
+
+}
+
+if (program.beets) {
+
+}
